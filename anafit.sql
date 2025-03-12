c607a7c4d764dcd31044b9707f25dffc1d314828
Zubiseru <jose.ehmendes@senacsp.edu.br>
2025-03-12 16:16:47 -0300

    feat: fazendo SELECT e FOREIGN KEY

--- anafit.sql
+++ anafit.sql
@@ -2,9 +2,9 @@ CREATE SCHEMA IF NOT EXISTS `anafit` DEFAULT CHARACTER SET utf8 ;
 USE `anafit`;
 
 -- -----------------------------------------------------
--- Tabela `anafit`.`tb_cliente`
+-- Tabela `anafit`.`cliente`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `anafit`.`tb_cliente` (
+CREATE TABLE IF NOT EXISTS `anafit`.`cliente` (
   `cpf` BIGINT NOT NULL,
   `primeironome` VARCHAR(50) NOT NULL,
   `sobrenome` VARCHAR(50) NOT NULL,
@@ -21,32 +21,31 @@ CREATE TABLE IF NOT EXISTS `anafit`.`tb_cliente` (
 ) ENGINE = InnoDB;
 
 -- -------------------------------------------------------
--- Tabela `anafit`.`tb_treino`
+-- Tabela `anafit`.`treino`
 -- -------------------------------------------------------
-CREATE TABLE IF NOT EXISTS `anafit`.`tb_treino` (
-  `cpf_cliente` BIGINT NOT NULL,
+CREATE TABLE IF NOT EXISTS `anafit`.`treino` (
   `diadasemana` DATE NOT NULL,
   `horario` TIME NOT NULL,
-  `tipo` VARCHAR(50),
-  PRIMARY KEY (`cpf_cliente`, `diadasemana`, `horario`),
-  CONSTRAINT `fk_cpf_cliente` FOREIGN KEY (`cpf_cliente`) REFERENCES `anafit`.`tb_cliente` (`cpf`) ON DELETE CASCADE
+  `tipo` VARCHAR(50)
 ) ENGINE = InnoDB;
 
+-- -------------------------------------------------------
+-- Tabela `anafit`.`pagamento`
+-- -------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `anafit`.`pagamento` (
+  `id_pagamento` INT AUTO_INCREMENT,
+  `valor` DECIMAL(5,2) NOT NULL,
+  `data_pagamento` DATE NOT NULL,
+  `cpf` BIGINT,
+  CONSTRAINT `fk_cpf` FOREIGN KEY (`cpf`) REFERENCES `anafit`.`cliente` (`cpf`),
+  PRIMARY KEY (`id_pagamento`)
+) ENGINE = InnoDB;
 
 -- -------------------------------------------------------
--- Inserts nas tabelas tb_cliente e tb_treino 
+-- INSERT nas tabelas cliente e treino 
 -- -------------------------------------------------------
 
-INSERT INTO `anafit`.`tb_cliente` 
-(`cpf`, `primeironome`, `sobrenome`, `datanasc`, `peso`, `altura`, `whatsapp`, `email`, `rua`, `numero`, `complemento`, `cep`) 
-VALUES
-(11122233300, 'Emilia', 'Tevere', '1992-03-11', 49.00, 1.50, '+5511998765432', 'emilia.tevere@lugunica.com', 'Floresta da Sombra', 301, 'Cabana de Gelo', '12345000'),
-(22233344411, 'Rem', 'Nox', '1993-09-02', 49.50, 1.55, '+5511987654321', 'rem.nox@mansaoroswaal.com', 'Avenida do Castelo', 404, 'Torre dos Demônios', '23456000'),
-(33344455522, 'Ram', 'Nox', '1993-09-02', 48.00, 1.54, '+5511976543210', 'ram.nox@mansaoroswaal.com', 'Rua da Tempestade', 505, 'Mansão dos Nox', '34567000'),
-(44455566633, 'Subaru', 'Natsuki', '1995-01-01', 70.00, 1.75, '+5511934567890', 'subaru.natsuki@lugunica.com', 'Avenida dos Heróis', 101, 'Casa de Dragon', '45678000'),
-(55566677744, 'Beatrice', 'Azazel', '1997-06-01', 40.00, 1.45, '+5511945678901', 'beatrice.azazel@bibliotecamistica.com', 'Biblioteca do Século', 303, 'Sala de Livros Mágicos', '56789000');
-
-INSERT INTO `anafit`.`tb_treino` 
+INSERT INTO `anafit`.`treino` 
 (`diadasemana`, `horario`, `tipo`) 
 VALUES
 ('2025-03-10', '08:00:00', 'Musculação'),
@@ -55,23 +54,116 @@ VALUES
 ('2025-03-13', '18:00:00', 'Ioga'),
 ('2025-03-14', '06:30:00', 'Ciclismo');
 
--- -------------------------------------------------------
--- Updates nas tabelas tb_cliente e tb_treino 
--- -------------------------------------------------------
+INSERT INTO `anafit`.`cliente` 
+(`cpf`, `primeironome`, `sobrenome`, `datanasc`, `peso`, `altura`, `whatsapp`, `email`, `rua`, `numero`, `complemento`, `cep`) 
+VALUES
+(12345678911, 'João', 'Silva', '1990-05-15', 72.30, 1.80, '+5511998765432', 'joao.silva@email.com', 'Rua das Flores', 120, 'Apt 301', '01234000'),
+(12345678912, 'Maria', 'Oliveira', '1985-10-22', 58.00, 1.65, '+5511987654321', 'maria.oliveira@email.com', 'Avenida Paulista', 1000, 'Bloco B', '01310000'),
+(12345678913, 'Carlos', 'Pereira', '1995-07-30', 85.50, 1.90, '+5511987432123', 'carlos.pereira@email.com', 'Rua dos Três Corações', 350, '', '01453000'),
+(12345678914, 'Beatriz', 'Santos', '2000-02-10', 50.70, 1.70, '+5511976543210', 'beatriz.santos@email.com', 'Rua da Paz', 555, 'Sala 502', '02540000'),
+(12345678915, 'Lucas', 'Costa', '1992-11-05', 77.40, 1.78, '+5511998764321', 'lucas.costa@email.com', 'Praça da Liberdade', 300, '', '03120000');
 
-UPDATE `anafit`.`tb_treino`
+INSERT INTO pagamento(`valor`, `data_pagamento`, `cpf`)
+VALUES
+(150.00, '2025-03-01', 12345678911), -- Pagamento do João
+(200.00, '2025-03-05', 12345678912), -- Pagamento da Maria
+(300.00, '2025-03-10', 12345678913), -- Pagamento do Carlos
+(250.00, '2025-03-02', 12345678911), -- Outro pagamento do João
+(100.00, '2025-03-03', 12345678915); -- Pagamento de Lucas
+
+-- -------------------------------------------------------
+-- UPDATE nas tabelas cliente e treino 
+-- -------------------------------------------------------
+UPDATE `anafit`.`treino`
 SET 
   `diadasemana` = '2024-03-14' 
 WHERE `tipo` = 'Musculação' AND `diadasemana` = "2025-03-10";
 
-UPDATE `anafit`.`tb_treino`
+UPDATE `anafit`.`treino`
 SET 
   `horario` = '15:00:00'
 WHERE `horario` IN('18:00:00', '06:30:00');
 
+UPDATE cliente
+SET peso = 66.50, datanasc = '1980-07-18'
+WHERE  cpf = 44455566633;
 
-DELETE FROM `anafit`.`tb_treino` WHERE `tipo` = 'Ciclismo';
+-- -------------------------------------------------------
+-- SELECT nas tabelas cliente e treino 
+-- -------------------------------------------------------
+SELECT * FROM `treino`; 
 
+SELECT tipo FROM `treino`;
+
+SELECT diadasemana, horario FROM `treino`;
+
+SELECT * FROM cliente
+WHERE datanasc BETWEEN '1992-01-01' AND '2000-12-31';
+
+SELECT * FROM cliente
+WHERE peso BETWEEN 66 AND 100;
+
+SELECT * FROM cliente
+WHERE peso = 58 OR peso = 100;
+
+-- -------------------------------------------------------
+-- SELECT(SOMAR) nas tabelas cliente e treino 
+-- -------------------------------------------------------
+SELECT SUM(peso) AS soma_peso FROM `cliente`;
+
+SELECT COUNT(*) AS total_registros FROM `cliente`;
+
+SELECT AVG(peso) AS media_peso FROM `cliente`;
+
+SELECT MAX(peso) AS maior_peso FROM `cliente`;
+
+SELECT MIN(peso) AS menor_peso FROM `cliente`;
+
+
+SELECT -- Combinando SELECT
+  SUM(peso) AS soma_peso,
+  COUNT(*) AS total_registros,
+  AVG(peso) AS media_peso,
+  MAX(peso) AS maior_peso
+  MIN(peso) AS menor_peso,
+FROM `cliente`; 
+
+-- -------------------------------------------------------
+-- SELECT MENOR E MAIOR nas tabelas cliente e treino 
+-- -------------------------------------------------------
+SELECT * FROM `cliente`
+WHERE altura > 1.78;
+
+SELECT * FROM `cliente`
+WHERE altura < 1.70;
+
+
+SELECT * FROM `cliente` -- Combinando SELECT 2
+WHERE altura > 1.78 OR altura < 1.70;
+
+SELECT
+  c.`cpf`,
+  c.`primeironome`,
+  c.`sobrenome`,
+  p.`valor`,
+  p.`data_pagamento`
+FROM 
+  `cliente` c
+INNER JOIN
+  `pagamento` p ON c.`cpf` = p.`cpf`
+WHERE
+  c.`cpf` = 12345678911;
+
+-- -------------------------------------------------------
+-- DELETE nas tabelas cliente e treino 
+-- -------------------------------------------------------
+DELETE FROM `anafit`.`treino` WHERE `tipo` = 'Ciclismo';
+
+DELETE * FROM `treino`;
+
+DROP TABLE `treino`;
+
+DROP DATABASE `anafit`;
 
 
 
